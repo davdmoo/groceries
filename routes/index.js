7fa90f11ba38f3442e8c6748874798da4c55f938
David <david.mulyawan97@gmail.com>
2021-11-18 11:32:55 +0700

    fixed routing

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const users = require('./userRoutes.js');
 const products = require('./productRoutes.js');
 
-// router.use('/', (req, res) => res.redirect);
+router.get('/', (req, res) => res.render('home'));
 
 router.use('/users', users);
 
-router.use('/', products);
+router.use('/products', products);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/productController.js');
 
-router.use('/', (req, res) => res.redirect('/products'));
+// router.use('/', (req, res) => res.redirect('/products'));
 
-router.use('/products', Controller.getProducts);
+router.get('/', Controller.getProducts);
 
-router.use('/update/:id', (req, res) => res.send('edit product page'));
+router.get('/update/:id', (req, res) => res.send('edit product page'));
 router.post('/update/:id', (req, res) => res.send('post edit product page'));
 
-router.use('/delete/:id', (req, res) => res.send('delete product page'));
+router.get('/delete/:id', (req, res) => res.send('delete product page'));
 
 module.exports = router;
